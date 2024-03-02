ae664b73b710c0dcc9d0fb97a1a2af139c444561
Kenneth <kennethyoung.2960@gmail.com>
2024-03-01 23:36:23 -0600

    Prac1 as short as can be

--- practice1.js
+++ practice1.js
@@ -1,22 +1,18 @@
 const location = {
-
     name: "Arches",
-  
     state: "Utah",
-  
     geo: {
-  
       lat: 38.68,
-  
       lon: -109.57,
-  
     },
-  
   };
-  
   
   const getLocationState = function (location) {
-  
     return location.state;
-  
   };
+// replace function with => after key
+  const getLocationStateShort = (location) => {
+    return location.state;
+  }
+// Why even make it two lines?
+  const getLocationStateRealShort = (location) => location.state
