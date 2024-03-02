e8d7f59b315669e1178cf0f4c0a623c438a63f8a
Kenneth <kennethyoung.2960@gmail.com>
2024-03-01 23:30:06 -0600

    Initial commit

---
+++ practice1.js
@@ -0,0 +1,22 @@
+const location = {
+
+    name: "Arches",
+  
+    state: "Utah",
+  
+    geo: {
+  
+      lat: 38.68,
+  
+      lon: -109.57,
+  
+    },
+  
+  };
+  
+  
+  const getLocationState = function (location) {
+  
+    return location.state;
+  
+  };
